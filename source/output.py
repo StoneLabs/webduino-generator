c26d0f24c7501fad077cd32bc9191d6edc361829
StoneLabs <git@sobadev.com>
2020-07-06 18:38:53 +0100

    Initial commit

---
+++ source/output.py
@@ -0,0 +1,21 @@
+from rich.console import Console
+console = Console()
+
+def Section(text: str) -> None:
+    console.print("[bold blue]::", "[bold white]" + text)
+
+def Text(text: str) -> None:
+    console.print(text)
+
+def Error(text: str) -> None:
+    console.print("[bold red]Error", text)
+    exit()
+
+if __name__ == "__main__":
+    Section("Section demo")
+    Text("Lorem ipsum")
+    Text("Lorem ipsum")
+    Text("Lorem ipsum")
+    Section("Section demo")
+    Text("Lorem ipsum")
+    Text("Lorem ipsum")
