6edaa0448717e1f19098589a06df86a0b8077bdb
StoneLabs <levyehrstein@googlemail.com>
2020-07-28 15:13:51 +0200

    Working generator

---
+++ templates/commands.h
@@ -0,0 +1,16 @@
+#pragma once
+
+inline void staticResponder(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete, const unsigned char* response, const char* mime)
+{
+  server.httpSuccess(mime);
+
+  /* if we're handling a GET or POST, we can output our data here.
+     For a HEAD request, we just stop after outputting headers. */
+  if (type != WebServer::ConnectionType::HEAD)
+  {
+    /* this is a special form of print that outputs from PROGMEM */
+    server.printP(response);
+  }
+}
+
+__COMMANDS_DEF__
---
+++ templates/main.wifi.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+// HotHix for WebServer on Nano 33 IoT
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_end(v) __builtin_va_end(v)
+#define WEBDUINO_FAIL_MESSAGE "<h1>400 Bad Request</h1>"
+
+#include "WebServer.h"
+
+#include "commands.h"
+
+WebServer webserver("", __PORT__);
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.begin("__SSID__", "__PASS__");
+    if ( WiFi.status() != WL_CONNECTED) 
+    {
+        Serial.println("Couldn't get a wifi connection");
+        while(true);
+    }
+    else
+        Serial.println(WiFi.localIP());
+
+    __COMMANDS_ADD__
+
+    webserver.begin();
+}
+
+void loop()
+{
+    webserver.processConnection();
+}
