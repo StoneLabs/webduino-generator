85d0b1049e3ea9587cd5d3b5a24a72816bc6f308
StoneLabs <git@sobadev.com>
2020-08-12 21:37:05 +0200

    Add long desription to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='webduino-generator',
-    version='0.1',
+    version='0.2',
+    license='UNLICENSE',
     url='https://github.com/StoneLabs/webduino-generator',
     author='Levy Ehrstein',
     author_email='levyehrstein@googlemail.com',
     description='Python program to automatically generate source code for an arduino web server',
+    long_description='Python program to automatically create arduino webserver from folder.\nSupports basically all file types. For example: html, js, css, images, and arbitrary binaries.',
     packages=["webduino_generator"],
     package_data={"webduino_generator": ["templates/*"]},
     include_package_data=True,
